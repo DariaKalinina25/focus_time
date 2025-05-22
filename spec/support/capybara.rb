9b382b8b7744988631adaafe0e8732a3a153e98b
DariaKalinina25 <meuw.meuw20@gmail.com>
2025-05-22 19:52:26 +0400

    chore: configure RSpec and Capybara setup

---
+++ spec/support/capybara.rb
@@ -0,0 +1,12 @@
+require 'capybara/cuprite'
+
+Capybara.register_driver :cuprite do |app|
+  Capybara::Cuprite::Driver.new(
+    app,
+    headless: true,
+    window_size: [1200, 800],
+    timeout: 10
+  )
+end
+
+Capybara.default_max_wait_time = 5
